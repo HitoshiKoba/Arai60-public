41cbbafc9c3046a80c66ead649ed9da8509c7fec
k0bak <jin.sisin1019@gmail.com>
2025-03-10 23:24:53 +0900

    Update step1.java

--- RemoveDuplicatesfromSortedList2/step1.java
+++ RemoveDuplicatesfromSortedList2/step1.java
@@ -1,1 +1,23 @@
-
+//did not run
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ //一個ずらし？　自分自身も消す必要　
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while(current != null && current.next != null) {
+            while (current.val == curent.next.val) {
+                current.next = current.next.next;
+                current = current.next
+            }
+        }
+    }
+}
