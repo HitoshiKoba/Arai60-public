ad33b1caecb5cb3c8dba38cb1419a80d1f56c78f
k0bak <jin.sisin1019@gmail.com>
2025-03-10 17:56:41 +0900

    Update step1.java

--- LinkedListCycle2/step1.java
+++ LinkedListCycle2/step1.java
@@ -1,1 +1,30 @@
-
+import java.util.ArrayList;
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ //戦略
+ //順序付き配列、Arrayリストに、メモリアドレスを格納する
+//サイクルが始まるところで、contains()で引っかかるはずだから、そこのarrayListのインデックスをgetする。その順番にあるListNodeを返したかった。
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ArrayList <ListNode> list = new ArrayList<>();
+        ListNode current = head;
+        while(current != null) {
+            if (list.contains(current)) {
+            int i = list.indexOf(current);
+            return ;
+            }
+            list.add(current);
+            current = current.next;
+        }
+        return null;
+    }
+}
