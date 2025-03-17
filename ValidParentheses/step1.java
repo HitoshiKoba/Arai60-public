e50c713678016227ca7e55b032765debbd4942a1
k0bak <jin.sisin1019@gmail.com>
2025-03-17 09:29:56 +0900

    Update step1.java

--- ValidParentheses/step1.java
+++ ValidParentheses/step1.java
@@ -1,1 +1,18 @@
+//閉じなきゃいけない[]
+//もし([{こうなったら、}])こうしなきゃいけない
+//スタック構造の実装
+//ポインタをずらす、ダンスのリズム（Last In, First Out）
+//        char 配列に変換して forEach で処理
+//        for (char c : str.toCharArray()) {
+//            System.out.println(c);
+//        }
+//再帰呼び出し？
 
+class Solution {
+    public boolean isValid(String s) {
+        int i = 0;
+        for (char c : s.toCharArray()) {
+            
+        }
+
+}
