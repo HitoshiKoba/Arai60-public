02f38d3b51ef4ab9d492f5ff5a6b9535098d8b5f
k0bak <jin.sisin1019@gmail.com>
2025-03-18 15:33:35 +0900

    Update step2.java

--- ReversedLinkedList/step2.java
+++ ReversedLinkedList/step2.java
@@ -13,13 +13,15 @@
  //
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;//0
-        ListNode curr = head;//1
+     //ポインタを付け替える = 変数に格納されている（変数が指している）メモリアドレスを変更する　命令文のみ。valは関係ないようだ。
+     //
+        ListNode prev = null;//prevにはnullを代入
+        ListNode curr = head;//currentは最初のノードインスタンスheadを指す
         while (curr != null) {
-            ListNode nextTemp = curr.next;//2
-            curr.next = prev;//2の位置に0を代入
-            prev = curr;//0の位置に1を代入
-            curr = nextTemp;//1の位置に２を代入
+            ListNode nextTemp = curr.next;//ノード：ネクスト一時的ノード。 
+            curr.next = prev;//2の位置に0を代入 //curr.nextがnullを指すようにする
+            prev = curr;//0の位置に1を代入　//prevがheadノードを指すようにする
+            curr = nextTemp;//1の位置に２を代入 //currが、nextTempが持っているやつ　＝　head.next、を指すようにする
         }
         return prev;
     }
