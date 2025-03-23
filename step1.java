3fbfde400fc0cbcfb4fcfc3d88a956184beadb5c
k0bak <jin.sisin1019@gmail.com>
2025-03-23 18:02:51 +0900

    Update step1.java

--- step1.java
+++ step1.java
@@ -17,9 +17,43 @@ class Solution {
         return 0;
     }
 }
+Pairはオブジェクト
+
+//step2.java
+import java.util.Random;
+class Solution {
+    private int[] cumulative;
+    private int total;
+    private Random rand;
+
+    public Solution(int[] w) {
+        cumulative = new int[w.length];
+        total = 0;      
+        //累積和作る
+        for (int i = 0; i < w.length ; i++) {
+            total += w[i];
+            cumulative[i] = total;
+        }
+        rand = new Random();
+    }
+    
+    public int pickIndex() {
+        int target = rand.nextInt(total);
+        //線形探索
+        int left = 0, right = cumulative.length -1;
+        for (int i = 0; i < cumulative.length; i++) {
+            if (target < cumulative[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
 
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(w);
  * int param_1 = obj.pickIndex();
  */
+
+    
