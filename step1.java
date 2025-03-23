1ad6bdd91e8c4d9c2429b1c57ca8d008d75f13c9
k0bak <jin.sisin1019@gmail.com>
2025-03-23 14:04:05 +0900

    Create step1.java

---
+++ step1.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+class Solution {
+    //数値の重みに従って（確率的に）インデックス（０から、配列サイズ−１まで）を取得するpickIndex()関数を実装せよ
+    int[] weightArray;
+
+    public Solution(int[] w) {
+        this.weightArray = w;
+    }
+    
+    public int pickIndex() {
+        List <Pair<String, Double>> weights = Arrays.asList(
+            new Pair<>()
+        )
+        w[i] = 
+        
+        return 0;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
