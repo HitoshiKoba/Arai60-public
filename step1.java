4488d0818cbbef57a1c54e23250e0276ac4cfec7
k0bak <jin.sisin1019@gmail.com>
2025-03-31 10:26:31 +0900

    Create step1.java

---
+++ step1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = {0,0};
+        boolean found = false;
+        outerloop:
+        for (int i= 0; i < nums.length; i++) {
+            Integer theOther = target - nums[i];
+            for (int j = i + 1 ; j < nums.length ; j++) {
+                if (theOther == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    found = true;
+                    break outerloop;
+                }
+            }
+        }
+        return result;
+
+    }
+}
+
+//コメント
+//boolean found = false というフラッグを使っているが、結局foundがfalseのままだった場合のreturn文の条件分岐を書いていないので、意味がないかもしれない。
